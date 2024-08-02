896ddaa228c98e80ba308e526f97aa0ccd6dcd05
willieJO <willie.oliveira@grxsolucoes.com.br>
2024-08-02 10:04:38 -0300

    passando o banco para postgress

--- api/rpg/src/main/resources/db/migration/V01__usuario.sql
+++ api/rpg/src/main/resources/db/migration/V01__usuario.sql
@@ -1,8 +1,6 @@
 CREATE TABLE Usuario (
-    `Id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   `Nome` varchar(250),
-  `Senha` varchar(250),
-  `Email` varchar(250) UNIQUE 
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
+    Id SERIAL PRIMARY KEY,
+    Nome VARCHAR(250),
+    Senha VARCHAR(250),
+    Email VARCHAR(250) UNIQUE
+);
--- api/rpg/src/main/resources/db/migration/V02__dados.sql
+++ api/rpg/src/main/resources/db/migration/V02__dados.sql
@@ -1,12 +1,12 @@
 CREATE TABLE rolagem (
-    `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `tipo_dado` varchar(250),
-    `multiplicador` varchar(250), 
-    `soma` varchar(250),
-    `resultado` varchar(250),
-    `resultado_modificacao` varchar(250),
-    `jogada_numero` int(11),
-    `add_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `usuario_id` INT(11),
-	CONSTRAINT FK_Rolagem_Usuario FOREIGN KEY (usuario_id) REFERENCES Usuario(Id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL PRIMARY KEY,
+    tipo_dado VARCHAR(250),
+    multiplicador VARCHAR(250), 
+    soma VARCHAR(250),
+    resultado VARCHAR(250),
+    resultado_modificacao VARCHAR(250),
+    jogada_numero INT,
+    add_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    usuario_id INT,
+    CONSTRAINT FK_Rolagem_Usuario FOREIGN KEY (usuario_id) REFERENCES Usuario(Id)
+);
