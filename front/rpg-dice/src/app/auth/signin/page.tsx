eaa6f1e2a38ffc0af1ef37bea00146472a6e76f1
willieJO <willie.oliveira@grxsolucoes.com.br>
2024-08-03 02:21:01 -0300

    gambiara pra cookie

--- front/rpg-dice/src/app/auth/signin/page.tsx
+++ front/rpg-dice/src/app/auth/signin/page.tsx
@@ -46,6 +46,7 @@ const SignIn: React.FC = () => {
     })
     .then((response : any) => {
       localStorage.setItem("accessToken", response.data.access_token);
+      document.cookie = 'refreshToken=' +  response.data.access_token;
       localStorage.setItem("name", response.data.name);
       router.push("/rolagem")
     }).catch((e:any) => {
